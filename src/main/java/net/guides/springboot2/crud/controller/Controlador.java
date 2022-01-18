a5e564ca40fe27c85743a2836a63b487208e84de
maxidc86 <max-rub@hotmail.com>
2022-01-18 20:24:35 -0300

    JSP configuration

--- src/main/java/net/guides/springboot2/crud/controller/Controlador.java
+++ src/main/java/net/guides/springboot2/crud/controller/Controlador.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class Controlador {
 
 	// --WELCOME PAGE -------------
