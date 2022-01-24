92f2cf3a74158da6ee686856d312ce30753de487
maxidc86 <max-rub@hotmail.com>
2022-01-23 21:23:18 -0300

    outlook smtp port from 465 to 587

--- src/main/java/net/guides/springboot2/crud/controller/SendMail.java
+++ src/main/java/net/guides/springboot2/crud/controller/SendMail.java
@@ -24,11 +24,11 @@ public class SendMail {
 		System.out.println("SSLEmail Start");
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp-mail.outlook.com"); //SMTP Host
-		props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
+		props.put("mail.smtp.socketFactory.port", "587"); //SSL Port
 		props.put("mail.smtp.socketFactory.class",
 				"javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
 		props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
-		props.put("mail.smtp.port", "465"); //SMTP Port
+		props.put("mail.smtp.port", "587"); //SMTP Port
 		
 		Authenticator auth = new Authenticator() {
 			//override the getPasswordAuthentication method
